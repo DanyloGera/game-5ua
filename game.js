b7126cce84e38476f9f9e98918629af69129469a
DanyloGera <64353215+DanyloGera@users.noreply.github.com>
2021-06-07 18:37:19 +0300

    adapt

--- game.js
+++ game.js
@@ -166,6 +166,10 @@ window.onload = function () {
 
   // before the game begins
   function preload() {
+    game.scale.scaleMode = Phaser.ScaleManager.USER_SCALE;
+    game.scale.setMinMax(700, 500, 900, 700);
+    game.scale.pageAlignHorizontally = true;
+    game.scale.pageAlignVertically = true;
 
     game.load.image('background', 'background.png');
 
